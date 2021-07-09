3a32253e49788582293fedf14d3cba3e3e89d08d
dbraunaleri <dominik.braun@aleri.de>
2021-07-09 14:48:59 +0200

    Scan component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QrscanComponent } from './qrscan/qrscan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrscanComponent
   ],
   imports: [
     BrowserModule
