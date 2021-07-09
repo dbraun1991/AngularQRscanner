d4362b59db3938d4e4b3f0b717cae84007e4290e
dbraunaleri <dominik.braun@aleri.de>
2021-07-09 15:09:04 +0200

    Add QR-Scanner to component

--- src/app/qrscan/qrscan.component.ts
+++ src/app/qrscan/qrscan.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BarcodeFormat, ResultMetadataType } from '@zxing/library';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-qrscan',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrscanComponent implements OnInit {
 
-  constructor() { }
+  public scannerEnabled: boolean = true;
+  public information: string = "No code information has been detected. Zoom in on a QR code to scan.";
+  allowedFormats = [ BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX /*, ...*/ ];
+
+  @ViewChild
+  (ZXingScannerComponent) qrscanner: ZXingScannerComponent;
+  
+  
+  constructor() {
+    var qrInput:string = "qrInput";
+  }
 
   ngOnInit(): void {
+    console.log("Scanner - Autostart");
+
+    this.qrscanner.enable;
+    this.qrscanner.autostart;
+    
+    console.log("SubscribeToScanner");
+
+    this.qrscanner.scanSuccess.subscribe(result => {
+      console.log(result);
+    });
+
+  }
+
+  public scanSuccessHandler($event: any) {
+    this.scannerEnabled = false;  
+    this.information = $event;
+  }
+  
+  public enableScanner() {
+    this.scannerEnabled = !this.scannerEnabled;
+    this.information = "No code information has been detected. Zoom in on a QR code to scan.";
   }
 
 }
